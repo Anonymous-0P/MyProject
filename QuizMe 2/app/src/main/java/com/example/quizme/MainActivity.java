7cead427e4e9acb61db389ea0615fa6d9bebc7a3
Anonymous-0P <120655334+Anonymous-0P@users.noreply.github.com>
2023-04-30 12:35:32 +0530

    Quiz App

---
+++ QuizMe 2/app/src/main/java/com/example/quizme/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.quizme;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.quizme.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, new HomeFragment());
+        transaction.commit();
+
+        binding.bottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                switch (i){
+                    case 0:
+                        transaction.replace(R.id.content, new HomeFragment());
+                        transaction.commit();
+                        break;
+                    case 1:
+                        transaction.replace(R.id.content, new LeaderboardsFragment());
+                        transaction.commit();
+                        break;
+                    case 2:
+                        transaction.replace(R.id.content, new WalletFragment());
+                        transaction.commit();
+                        break;
+                    case 3:
+                        transaction.replace(R.id.content, new ProfileFragment());
+                        transaction.commit();
+                        break;
+                }
+                return false;
+            }
+        });
+            }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.Logout) {
+            Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+            logout = (Button) findViewById(R.id.Logout);
+            logout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent( MainActivity.this, SignupActivity.class ));
+                }
+            });
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
